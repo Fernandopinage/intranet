3ecde30d417fb1b854e3702880691749d498a0f9
FernandoPinage <luizfernandoluck@hotmail.com>
2021-05-14 15:27:47 -0400

    index ok

--- app/php/index.php
+++ app/php/index.php
@@ -27,7 +27,7 @@
             </div>
 
             <div class="d-grid gap-2">
-            <button class="btn btn-primary" type="button">Button</button>
+            <button class="btn" type="button">Button</button>
             </div>
 
         </form>
